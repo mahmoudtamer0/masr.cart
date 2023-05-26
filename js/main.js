e776b55e67cf98689caf82c8fd5768f0dc316f23
mahmoud <mamoidtamer300@gmail.com>
2023-05-26 04:16:03 +0300

    deploy products

--- js/main.js
+++ js/main.js
@@ -56,3 +56,79 @@ logoutbtn.onclick = function () {
         window.location = 'log-in.html';
     }, 1500)
 }
+
+// main page
+let productsDOM = document.querySelector('.products-items');
+let products = [
+    {
+        id: 1,
+        imgsrcUrl: 'images/download.jpeg',
+        itemTitle: 'camera',
+        itemDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        itemPrice: '$5000',
+        itemSize: 'Large',
+    },
+    {
+        id: 2,
+        imgsrcUrl: 'images/download.jpeg',
+        itemTitle: 'headphone sodo sd-1004',
+        itemDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        itemPrice: '$1500',
+        itemSize: 'Large',
+    },
+    {
+        id: 3,
+        imgsrcUrl: 'images/download.jpeg',
+        itemTitle: 'makeup',
+        itemDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        itemPrice: '$500',
+        itemSize: 'Small'
+    },
+    {
+        id: 4,
+        imgsrcUrl: 'images/download.jpeg',
+        itemTitle: 'Rader',
+        itemDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        itemPrice: '$2000',
+        itemSize: 'Large'
+    },
+    {
+        id: 5,
+        imgsrcUrl: 'images/download.jpeg',
+        itemTitle: 'camera pro',
+        itemDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        itemPrice: '$8000',
+        itemSize: 'Large'
+    },
+    {
+        id: 6,
+        imgsrcUrl: 'images/download.jpeg',
+        itemTitle: 'headphone sodo sd-1007',
+        itemDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        itemPrice: '$9000',
+        itemSize: 'Large'
+    }
+];
+
+function drawingUiProducts() {
+    let productsUI = products.map((item) => {
+        return `
+                    <div class="item col-lg-4">
+                        <div class="item-img">
+                            <img src="${item.imgsrcUrl}" alt="">
+                        </div>
+                        <div class="item-description">
+                            <h2>${item.itemTitle}</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            <span>Size: ${item.itemSize}</span>
+                            <span>Price: ${item.itemPrice}</span>
+                            <div class="item-add">
+                                <button>Add to cart</button>
+                            </div>
+                        </div>
+                    </div>
+        `
+    })
+    productsDOM.innerHTML = productsUI;
+};
+drawingUiProducts();
